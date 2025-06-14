ac96e19ae4b5eddb628142c53236d240e37c37f8
Josue LC123 <darkraider705@gmail.com>
2025-06-13 22:42:16 -0500

    Proyecto terminado al 95%

--- service/InicioService.js
+++ service/InicioService.js
@@ -8,6 +8,26 @@ import { formatDestino } from '../helpers/destinoFormatter.js' // asegúrate de 
 export class InicioService {
   static async getInicioData () {
     const [destinosRaw, lugares, servicios, duracion] = await Promise.all([
+      DestinosModel.getAll(), // obtiene todos los destinos activos y próximos
+      LugaresModel.getAll(),
+      ServicioModel.getAll(),
+      DuracionModel.getAll()
+    ]);
+
+    const destinos = formatDestino(destinosRaw); // aplica el formateo
+
+    // Divide destinos según estado
+    const favoritos = destinos.filter(dest => dest.estado === 'activo');
+    const proximamente = destinos.filter(dest => dest.estado === 'proximo');
+
+    return { lugares, servicios, duracion, favoritos, proximamente, testimonials };
+  }
+}
+
+
+/*export class InicioService {
+  static async getInicioData () {
+    const [destinosRaw, lugares, servicios, duracion] = await Promise.all([
       DestinosModel.getAll(),
       LugaresModel.getAll(),
       ServicioModel.getAll(),
@@ -18,28 +38,5 @@ export class InicioService {
 
     return { lugares, servicios, duracion, destinos, testimonials };
   }
-}
-
-/*import { DestinosModel } from '../models/Destinos.js'
-import { DuracionModel } from '../models/Duracion.js'
-import { LugaresModel } from '../models/Lugares.js'
-import { ServicioModel } from '../models/Servicio.js'
-import { testimonials } from '../const/const.js'
-import { formatDestino } from '../helpers/destinoFormatter.js'
-
-export class InicioService {
-  static async getInicioData () {
-    const [destinosRaw, lugares, servicios, duracion, proximosRaw] = await Promise.all([
-      DestinosModel.getAll(),
-      LugaresModel.getAll(),
-      ServicioModel.getAll(),
-      DuracionModel.getAll(),
-      DestinosModel.getProximos()
-    ])
-
-    const destinos = formatDestino(destinosRaw)
-    const proximos = formatDestino(proximosRaw)
-
-    return { lugares, servicios, duracion, destinos, proximos, testimonials }
-  }
 }*/
+
